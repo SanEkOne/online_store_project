fb930e3f8840d6dbf59cfd1ed2533f28acf8efff
SanEk <tt.worm524@gmail.com>
2025-11-26 16:53:12 +0200

    added order form

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-class PrintProducts{
+class PrintProducts {
   constructor(containerId) {
     this.container = document.getElementById(containerId);
   }
 
-  loadItems() {
+  loadItems(minPrice, maxPrice) {
     const products = JSON.parse(localStorage.getItem("products") || "[]");
     this.container.innerHTML = "";
 
@@ -11,6 +11,9 @@ class PrintProducts{
       let link = document.createElement("a");
       link.href = `Pages/product_page.html?name=${encodeURIComponent(product.name)}`;
       link.className = "product_link";
+
+      minPrice = Number(minPrice) || 1;
+      maxPrice = Number(maxPrice) || 1000;
 
       let mainPhoto = product.photo1;
 
@@ -22,15 +25,69 @@ class PrintProducts{
         <p>${product.price} ₴</p>
         `;
 
-      link.appendChild(div);
-      this.container.appendChild(link);
+      if (product.price >= minPrice && product.price <= maxPrice) {
+        link.appendChild(div);
+        this.container.appendChild(link);
+      }
 
+      // link.appendChild(div);
+      // this.container.appendChild(link);
     })
   }
 }
 
+class Filter {
+  constructor() {
+    this.price_range = document.getElementById("price_range");
+    this.min_price = document.getElementById("min_price_input");
+    this.max_price = document.getElementById("max_price_input");
+    this.submitBTN = document.getElementById("submitBTN");
+
+    this.submitBTN.addEventListener("click", () => this.startFilter());
+    printProducts.loadItems(this.min_price.value, this.max_price.value);
+    this.price_range.addEventListener("input", () => this.displayInputPrice());
+    this.min_price.addEventListener("input", () => this.validatePrices());
+    this.max_price.addEventListener("input", () => this.validatePrices());
+    this.displayInputPrice()
+
+  }
+
+  startFilter(){
+    printProducts.loadItems(this.min_price.value, this.max_price.value);
+
+  }
+
+  displayInputPrice() {
+    this.max_price.value = this.price_range.value;
+    this.price_range.min = this.min_price.value;
+  }
+
+  validatePrices() {
+    this.min_price.value = this.min_price.value.replace(/\D/g, "");
+    this.max_price.value = this.max_price.value.replace(/\D/g, "");
+
+    let min = Number(this.min_price.value);
+    let max = Number(this.max_price.value);
+
+    if (min < 1) min = 1;
+    if (max > 1000) max = 1000;
+
+    if (min > max) {
+      min = 1;
+      max = 1000;
+    }
+
+    this.min_price.value = min;
+    this.max_price.value = max;
+
+    this.price_range.min = min;
+    this.price_range.max = max;
+  }
+}
+
 const printProducts = new PrintProducts("products_container");
-printProducts.loadItems();
+const filter = new Filter();
 
-
+// localStorage.removeItem("products");
+// localStorage.removeItem("basket_products");
 
--- js/basket.js
+++ js/basket.js
@@ -1,16 +1,23 @@
-class PrintBasketProducts{
+class PrintBasketProducts {
+  totalPrice = 0;
   constructor(containerId) {
-    this.container = document.getElementById(containerId);
+    this.basket_container = document.getElementById(containerId);
   }
 
   loadItems() {
     const products = JSON.parse(localStorage.getItem("basket_products") || "[]");
-    this.container.innerHTML = "";
+    this.basket_container.innerHTML = "";
+    this.totalPrice = 0;
+
+    let totalPriceElement = document.getElementById("total_price");
 
     products.forEach(product => {
+      let count = 1;
       let link = document.createElement("a");
       link.href = `../Pages/product_page.html?name=${encodeURIComponent(product.name)}`;
       link.className = "product_link";
+
+      this.totalPrice += Number(product.price);
 
       let mainPhoto = product.photo1;
 
@@ -22,12 +29,112 @@ class PrintBasketProducts{
         <p>${product.price} ₴</p>
         `;
 
+      let amount_div = document.createElement("div");
+      let totalProductPrice = Number(product.price);
+      amount_div.className = "product_amount";
+      amount_div.innerHTML = `
+        <button class="addBTN">+</button>
+        <p class="count">${count}</p>
+        <button class="removeBTN">-</button>
+        <h1 class="productPrice">Цена: ${totalProductPrice}</h1>`;
+
+      let container = document.createElement("div");
+      container.className = "product_price_amount_container";
+
+
+
+      let addBtn = amount_div.querySelector(".addBTN");
+      let removeBtn = amount_div.querySelector(".removeBTN");
+      let countEl = amount_div.querySelector(".count");
+      let priceEl = amount_div.querySelector(".productPrice");
+
+
+      addBtn.addEventListener("click", () => {
+        count++;
+        countEl.textContent = count;
+        totalProductPrice += Number(product.price);
+        priceEl.textContent = `Цена: ${totalProductPrice} ₴`;
+
+        this.totalPrice += Number(product.price);
+        totalPriceElement.textContent = "Итоговая цена: " + this.totalPrice + " ₴";
+      });
+
+      removeBtn.addEventListener("click", () => {
+        if (count > 1) {
+          count--;
+          countEl.textContent = count;
+          totalProductPrice -= Number(product.price);
+          priceEl.textContent = `Цена: ${totalProductPrice} ₴`;
+
+          this.totalPrice -= Number(product.price);
+          totalPriceElement.textContent = "Итоговая цена: " + this.totalPrice + " ₴";
+        }
+      });
+
+
+
       link.appendChild(div);
-      this.container.appendChild(link);
+      container.appendChild(link);
+      container.appendChild(amount_div);
+
+      this.basket_container.appendChild(container);
 
     })
+    document.getElementById("total_price").textContent = `Итоговая цена: ${this.totalPrice} ₴`;
+  }
+
+}
+
+const printProducts = new PrintBasketProducts("product");
+printProducts.loadItems();
+
+
+
+
+class OrderForm {
+  constructor(){
+    this.ukrPoshtaRadio = document.getElementById("ukrposhta");
+    this.novaPoshtaRadio = document.getElementById("novaposhta");
+    this.pickupRadio = document.getElementById("pickup");
+
+    this.ukrPoshtaDeliveryContainer = document.getElementById("ukrposhta_delivery_container");
+    this.novaPoshtaDeliveryContainer = document.getElementById("novaposhta_delivery_container");
+    this.pickupDeliveryContainer = document.getElementById("pickup_delivery_container");
+
+    this.hideAll();
+
+    document.querySelectorAll('input[name="delivery"]').forEach(radio => {
+      radio.addEventListener('change', () => this.chooseDeliveryMethod());
+    });
+  }
+
+  hideAll() {
+    const containers = [
+      this.ukrPoshtaDeliveryContainer,
+      this.novaPoshtaDeliveryContainer,
+      this.pickupDeliveryContainer
+    ];
+    containers.forEach(container => {
+      if (container) container.style.display = "none";
+    });
+  }
+
+  chooseDeliveryMethod(){
+    this.hideAll();
+
+    if (this.ukrPoshtaRadio?.checked) {
+      this.ukrPoshtaDeliveryContainer.style.display = "flex";
+    } else if (this.novaPoshtaRadio?.checked) {
+      this.novaPoshtaDeliveryContainer.style.display = "flex";
+    } else if (this.pickupRadio?.checked) {
+      this.pickupDeliveryContainer.style.display = "flex";
+    }
   }
 }
 
-const printProducts = new PrintBasketProducts("basket_container");
-printProducts.loadItems();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const orderForm = new OrderForm();
+});
+
+
